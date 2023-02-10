8d928b8e98b70b98129be6a2f8704563a429ca62
MaymoonaAlBoloshi <maymoona.al.boloshi@gmail.com>
2023-02-10 13:20:27 +0400

    6.3 handle user inputs with express validator for put and post  routes

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body, oneOf, validationResult } from 'express-validator';
+import { handleInputError } from './modules/midldleware';
 const router = Router();
 
 /*
@@ -10,17 +11,30 @@ router.get('/products', (req, res) => {
 	res.status(200);
 	res.json({ message: req.abc, products: [] });
 });
+
 router.get('/products/:id', (req, res) => {});
-router.put('/products/:id', body('name').isString(), (req, res) => {
-	const errors = validationResult(req);
-	console.log(errors);
-	if (!errors.isEmpty()) {
-		return res.status(400).json({ errors: errors.array() });
+
+router.put(
+	'/products/:id',
+	body('name').isString(),
+	handleInputError,
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		res.status(200);
+		res.json({ message: 'success' });
 	}
-	res.status(200);
-	res.json({ message: 'success' });
-});
-router.post('/products', (req, res) => {});
+);
+
+router.post(
+	'/products',
+	body('name').isString(),
+	handleInputError,
+	(req, res) => {}
+);
+
 router.delete('/products/:id', (req, res) => {});
 
 /*
@@ -29,8 +43,22 @@ router.delete('/products/:id', (req, res) => {});
 
 router.get('/updates', (req, res) => {});
 router.get('/updates/:id', (req, res) => {});
-router.put('/updates/:id', (req, res) => {});
-router.post('/updates', (req, res) => {});
+router.put(
+	'/updates/:id',
+	body('title').optional(),
+	body('body').optional(),
+	body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
+	body('version').optional(),
+	handleInputError,
+	(req, res) => {}
+);
+router.post(
+	'/updates',
+	body('title').exists().isString(),
+	body('body').exists().isString(),
+	handleInputError,
+	(req, res) => {}
+);
 router.delete('/updates/:id', (req, res) => {});
 
 /*
@@ -39,8 +67,21 @@ router.delete('/updates/:id', (req, res) => {});
 
 router.get('/update-points', (req, res) => {});
 router.get('/update-points/:id', (req, res) => {});
-router.put('/update-points/:id', (req, res) => {});
-router.post('/update-points', (req, res) => {});
+router.put(
+	'/update-points/:id',
+	body('name').optional().isString(),
+	body('description').optional().isString(),
+	handleInputError,
+	(req, res) => {}
+);
+router.post(
+	'/update-points',
+	body('name').isString(),
+	body('description').isString(),
+	body('updateId').isString(),
+	handleInputError,
+	(req, res) => {}
+);
 router.delete('/update-points/:id', (req, res) => {});
 
 export default router;
