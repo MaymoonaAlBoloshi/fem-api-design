78516f61f27a222009dad37df8845ccbfac6c865
MaymoonaAlBoloshi <maymoona.al.boloshi@gmail.com>
2023-02-11 13:45:31 +0400

    update, create, and delete handlers for product

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -6,12 +6,11 @@ export const getProducts = async (req, res) => {
 			id: req.user.id,
 		},
 		include: {
-			// TODO update Product to products in prisma then everywhere else
-			Product: true,
+			products: true,
 		},
 	});
 
-	res.json({ data: user.Product });
+	res.json({ data: user.products });
 };
 
 export const getProduct = async (req, res) => {
@@ -25,4 +24,46 @@ export const getProduct = async (req, res) => {
 	});
 
 	res.json({ data: product });
+};
+
+export const createProduct = async (req, res) => {
+	const product = await prisma.product.create({
+		data: {
+			name: req.body.name,
+			belongToId: req.user.id,
+		},
+	});
+
+	res.json({ data: product });
+};
+
+export const updateProduct = async (req, res) => {
+	const updated = await prisma.product.update({
+		where: {
+			id: req.params.id,
+			id_belongToId: {
+				id: req.user.id,
+				belongToId: req.user.id,
+			},
+		},
+		data: {
+			name: req.body.name,
+		},
+	});
+
+	res.json({ data: updated });
+};
+
+export const deleteProduct = async (req, res) => {
+	const deleted = await prisma.product.delete({
+		where: {
+			id: req.params.id,
+			id_belongToId: {
+				id: req.user.id,
+				belongToId: req.user.id,
+			},
+		},
+	});
+
+	res.json({ data: deleted });
 };
